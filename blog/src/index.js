06d3ef4f93aa2133455d7051f6e6465ba6959cdf
kanggicheon <snoopy1516@naver.com>
2023-07-25 18:05:52 +0900

    리덕스 함수 추가, 적용

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
