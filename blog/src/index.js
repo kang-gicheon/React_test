ffca237c88c3fba4ffeeba25120461f73201a51c
kanggicheon <snoopy1516@naver.com>
2023-07-27 17:38:23 +0900

    회원가입, 로그인 기능 구현 리덕스 제작

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,13 +1,21 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import App from './App';
 import './index.css';
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './modules/index';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
