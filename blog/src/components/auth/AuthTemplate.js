615352e17947833196248410bda4f26e8eb67145
kanggicheon <snoopy1516@naver.com>
2023-07-27 09:08:00 +0900

    Auth 템플릿 로그인 페이지
    기본 레이아웃

--- blog/src/components/auth/AuthTemplate.js
+++ blog/src/components/auth/AuthTemplate.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
+import { Link } from 'react-router-dom';
 
 /**
  * 회원가입 / 로그인 페이지의 레이아웃을 담당하는 컴포넌트
@@ -16,10 +17,39 @@ const AuthTemplateBlock = styled.div`
 
   /* flext 속성을 이용한 내용 중앙 정렬 */
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+// 흰색 박스
+
+const WhiteBox = styled.div`
+  .logo-area {
+    display: block;
+    padding-bottom: 2rem;
+    text-align: center;
+    font-weight: bold;
+    letter-spacing: 2px;
+  }
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.025);
+  padding: 2rem;
+  width: 360px;
+  background: white;
+  border-radius: 2px;
 `;
 
 const AuthTemplate = ({ children }) => {
-  return <AuthTemplateBlock>{children}</AuthTemplateBlock>;
+  return (
+    <AuthTemplateBlock>
+      <WhiteBox>
+        <div className="logo-area">
+          <Link to="/">REACTERS</Link>
+        </div>
+        {children}
+      </WhiteBox>
+    </AuthTemplateBlock>
+  );
 };
 
 export default AuthTemplate;
